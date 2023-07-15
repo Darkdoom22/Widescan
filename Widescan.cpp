777644a511910ee498b224728764cce925cc398f
Audrey Riley <darkdoom1337@gmail.com>
2023-07-15 10:05:08 -0700

    Reading WidescanInfo* manually -> using a game function to get pointer, more robust (lua will need to check if the table is nil now as this function can return nullptr)
    additional error mesages

--- Widescan.cpp
+++ Widescan.cpp
@@ -4,26 +4,37 @@
 #include "Scanner.h"
 #include "LuaCoreWrapper.h"
 
+//struct internal to GlobalNowZone struct 
+struct WidescanInfo
+{
+	uint32_t Status;
+	float X;
+	float Z;
+	float Y;
+	uint16_t Index;
+	uint16_t Level;
+	uint32_t _unk1;
+	//theres another uint32_t here, not sure what it is, maybe related
+};
+
 const auto gcTrackingStartSetPattern = "\xa1\x00\x00\x00\x00\x85\xc0\x74\x00\x6a\x00\x6a\x00\x68\xf5\x00\x00\x00\xe8";
 const auto gcTrackingStartSetMask = "x????xxx?xxxxxxxxxx";
 
 const auto gcTrackingStopSetPattern = "\xa1\x00\x00\x00\x00\x85\xc0\x74\x00\x6a\x00\x6a\x00\x68\xf6\x00\x00\x00\xe8";
 const auto gcTrackingStopSetMask = "x????xxx?xxxxxxxxxx";
 
-const auto pGlobalNowZonePattern = "\x8D\x14\x80\x8D\x04\x50\x8B\x15\x00\x00\x00\x00\x8D\xBC\x82\x60\x98";
-const auto pGlobalNowZoneMask = "xxxxxxxx????xxxxx";
-constexpr auto pGlobalNowZoneOffset = 0x8;
+const auto gcTrackingPosGetPattern = "\xa1\x00\x00\x00\x00\x85\xc0\x74\x00\xf7\x80\x00\x00\x00\x00\xff\xff\x00\x00";
+const auto gcTrackingPosMask = "x????xxx?xx????xxxx";
 
-const auto WidescanInfoOffsetPattern = "\xb0\x01\x66\x89\xB2\x00\x00\x00\x00\x5e\xc3";
-const auto WidescanInfoOffsetMask = "xxxxx????xx";
-
-const auto WidescanInfoOffset = *reinterpret_cast<int32_t*>(FindPattern::ScanModIn(
-	const_cast<char*>(WidescanInfoOffsetPattern),
-	const_cast<char*>(WidescanInfoOffsetMask),
-	"FFXiMain.dll") + 5);
-
+using fnTrackingPosGet = WidescanInfo*(__stdcall*)();
 using fnTrackingStartSet = bool(__cdecl*)(uint32_t);
 using fnTrackingStopSet = bool(__cdecl*)();
+
+//returns a pointer to current widescan info
+const auto oTrackingPosGet = reinterpret_cast<fnTrackingPosGet>(FindPattern::ScanModIn(
+	const_cast<char*>(gcTrackingPosGetPattern),
+	const_cast<char*>(gcTrackingPosMask),
+	"FFXiMain.dll"));
 
 //outgoing 0xF5 packet handler
 const auto oTrackingStartSet = reinterpret_cast<fnTrackingStartSet>(
@@ -39,23 +50,6 @@ const auto oTrackingStopSet = reinterpret_cast<fnTrackingStopSet>(
 		const_cast<char*>(gcTrackingStopSetMask),
 		"FFXiMain.dll"));
 
-auto pGlobalNowZone = reinterpret_cast<uint32_t>(FindPattern::ScanModIn(
-	const_cast<char*>(pGlobalNowZonePattern),
-	const_cast<char*>(pGlobalNowZoneMask),
-	"FFXiMain.dll") + pGlobalNowZoneOffset);
-
-//struct internal to GlobalNowZone struct 
-struct WidescanInfo
-{
-	uint32_t Status;
-	float X;
-	float Z;
-	float Y;
-	uint16_t Index;
-	uint16_t Level;
-	//theres another uint32_t here, not sure what it is, maybe related
-};
-
 static int GetWidescanInfo(lua_State* L)
 {
 	if (LuaCoreWrapper::oLua_GetTop(L) != 0)
@@ -63,38 +57,37 @@ static int GetWidescanInfo(lua_State* L)
 		return LuaCoreWrapper::oLuaL_Error(L, "GetWidescanInfo takes no arguments");
 	}
 
-	if(pGlobalNowZone)
+	if (!oTrackingPosGet)
 	{
-		const auto pNowZone = *reinterpret_cast<uintptr_t*>(pGlobalNowZone);
-		if(pNowZone)
-		{
-			const auto nowZone = *reinterpret_cast<uintptr_t*>(pNowZone);
-			const auto widescanInfo = reinterpret_cast<WidescanInfo*>((nowZone + WidescanInfoOffset) - 0x10);
-
-			LuaCoreWrapper::oLua_NewTable(L);
-
-			LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Status);
-			LuaCoreWrapper::oLua_SetField(L, -2, "Status");
-
-			LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->X);
-			LuaCoreWrapper::oLua_SetField(L, -2, "X");
-
-			LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Z);
-			LuaCoreWrapper::oLua_SetField(L, -2, "Z");
-
-			LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Y);
-			LuaCoreWrapper::oLua_SetField(L, -2, "Y");
-
-			LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Index);
-			LuaCoreWrapper::oLua_SetField(L, -2, "Index");
-
-			LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Level);
-			LuaCoreWrapper::oLua_SetField(L, -2, "Level");
-
-			return 1;
-		}
-
+		return LuaCoreWrapper::oLuaL_Error(L, "GetWidescanInfo failed to find function oTrackingPosGet, the signature may have changed");
 	}
+
+	const auto widescanInfo = oTrackingPosGet();//only returns a pointer when there is a valid track
+	if (widescanInfo)
+	{
+		LuaCoreWrapper::oLua_NewTable(L);
+
+		LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Status);
+		LuaCoreWrapper::oLua_SetField(L, -2, "Status");
+
+		LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->X);
+		LuaCoreWrapper::oLua_SetField(L, -2, "X");
+
+		LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Z);
+		LuaCoreWrapper::oLua_SetField(L, -2, "Z");
+
+		LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Y);
+		LuaCoreWrapper::oLua_SetField(L, -2, "Y");
+
+		LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Index);
+		LuaCoreWrapper::oLua_SetField(L, -2, "Index");
+
+		LuaCoreWrapper::oLua_PushNumber(L, widescanInfo->Level);
+		LuaCoreWrapper::oLua_SetField(L, -2, "Level");
+
+		return 1;
+	}
+
 	return 0;
 }
 
@@ -107,7 +100,7 @@ static int TrackingStartSet(lua_State* L)
 
 	if (!oTrackingStartSet)
 	{
-		return 0;
+		return LuaCoreWrapper::oLuaL_Error(L, "TrackingStartSet failed to find function oTrackingStartSet, the signature may have changed");
 	}
 
 	const auto trackingStartSet = oTrackingStartSet(LuaCoreWrapper::oLuaL_CheckInteger(L, 1));
@@ -124,7 +117,7 @@ static int TrackingStopSet(lua_State* L)
 
 	if (!oTrackingStopSet)
 	{
-		return 0;
+		return LuaCoreWrapper::oLuaL_Error(L, "TrackingStopSet failed to find function oTrackingStopSet, the signature may have changed");
 	}
 
 	const auto trackingStopSet = oTrackingStopSet();
